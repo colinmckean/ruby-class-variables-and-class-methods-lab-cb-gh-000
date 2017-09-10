3b0c1c38f47ef574967b87b014066434be751a17
Colin McKean <ezikill@hotmail.com>
2017-09-10 09:35:09 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,7 +9,10 @@ class Song
     @artist = artist
     @genre = genre
     @@count += 1
-    @@artists << artist
+    if !@@artists.include? artist {
+      @@artists << artist
+    }
+
     @@genres << genre
   end
 
