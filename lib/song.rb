e3f6340caa4355fbf18204e2d9b06c3fdc3fe3dd
Colin McKean <ezikill@hotmail.com>
2017-09-10 09:56:53 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -29,7 +29,7 @@ class Song
   def Song.genre_count
     Hash[genres.map { |element|
       if [element] != nil
-        [element = 2]
+        [element, 2]
       else
         [element, 1]
       end
