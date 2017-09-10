cfab937453d29f67a600a41ca040708c539fbccb
Colin McKean <ezikill@hotmail.com>
2017-09-10 09:27:25 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,10 +6,11 @@ class Song
     @name = name
     @artist = artist
     @genre = genre
+    count += 1
 
   end
 
-  def class_variable_get(variable)
-    return this.variable    
-  end
+  # def class_variable_get(variable)
+  #   return this.variable
+  # end
 end
