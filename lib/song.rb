4b74ba259f98257f4d624c3a48a9082bbdaca2d3
Colin McKean <ezikill@hotmail.com>
2017-09-10 09:29:05 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,12 +2,15 @@ class Song
   attr_reader :name, :artist, :genre
   attr_accessor :count
   @@count = 0
+  @@artists = []
+  @@genres = []
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
-    @@count += 1
-
+    @@counts += 1
+    @@artist << artist
+    @@genres << genre
   end
 
   # def class_variable_get(variable)
