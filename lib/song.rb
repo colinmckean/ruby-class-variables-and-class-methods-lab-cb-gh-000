dbcb31e0650ebf9a24dbd7e7f954046610ddc5bd
Colin McKean <ezikill@hotmail.com>
2017-09-10 09:36:39 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,7 +6,7 @@ class Song
   @@genres = []
   def initialize(name, artist, genre)
     @name = name
-    # @artist = artist
+    @artist = artist
     @genre = genre
     @@count += 1
     if !@@artists.include? artist
